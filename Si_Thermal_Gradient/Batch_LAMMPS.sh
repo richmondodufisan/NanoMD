30f05a5dcaeeb1687ccd0a155ada95a550b16449
Alesanmi Richmond Odufisan <vtw1026@quser31.quest.it.northwestern.edu>
2024-10-21 15:02:33 -0500

    changed memory requested

--- Si_Thermal_Gradient/Batch_LAMMPS.sh
+++ Si_Thermal_Gradient/Batch_LAMMPS.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#SBATCH --account=pXXXXX  ## YOUR ACCOUNT pXXXX or bXXXX
+#SBATCH --account=p32089  ## YOUR ACCOUNT pXXXX or bXXXX
 #SBATCH --partition=short  ### PARTITION (buyin, short, normal, etc)
 #SBATCH --nodes=2 ## how many computers do you need
 #SBATCH --ntasks-per-node=4 ## how many cpus or processors do you need on each computer
 #SBATCH --time=00:20:00 ## how long does this need to run (remember different partitions have restrictions on this param)
-#SBATCH --mem-per-cpu=3G ## how much RAM do you need per CPU (this effects your FairShare score so be careful to not ask for more than you need))
+#SBATCH --mem-per-cpu=500M ## how much RAM do you need per CPU (this effects your FairShare score so be careful to not ask for more than you need))
 #SBATCH --job-name=silicon_simulation  ## When you run squeue -u NETID this is how you can identify the job
 #SBATCH --output=outlog_lammps_openmpi_intel ## standard out and standard error goes to this file
 #SBATCH --constraint="[quest8|quest9|quest10|quest11]"
