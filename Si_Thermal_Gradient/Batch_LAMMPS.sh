523b54b347f36ece2363374d2254252ba71624fe
Alesanmi Richmond Odufisan <vtw1026@quser32.quest.it.northwestern.edu>
2024-10-21 20:32:56 -0500

    updated batch file

--- Si_Thermal_Gradient/Batch_LAMMPS.sh
+++ Si_Thermal_Gradient/Batch_LAMMPS.sh
@@ -4,7 +4,7 @@
 #SBATCH --nodes=2 ## how many computers do you need
 #SBATCH --ntasks-per-node=4 ## how many cpus or processors do you need on each computer
 #SBATCH --time=04:00:00 ## how long does this need to run (remember different partitions have restrictions on this param)
-#SBATCH --mem-per-cpu=500M ## how much RAM do you need per CPU (this effects your FairShare score so be careful to not ask for more than you need))
+#SBATCH --mem-per-cpu=3G ## how much RAM do you need per CPU (this effects your FairShare score so be careful to not ask for more than you need))
 #SBATCH --job-name=silicon_simulation  ## When you run squeue -u NETID this is how you can identify the job
 #SBATCH --output=outlog_lammps_openmpi_intel ## standard out and standard error goes to this file
 #SBATCH --constraint="[quest8|quest9|quest10|quest11]"
@@ -12,5 +12,5 @@
 module purge
 module load lammps/20200303-openmpi-4.0.5-intel-19.0.5.281
 
-mpirun -np ${SLURM_NTASKS} lmp -in Si_Bulk_Hot_Cold_ase.lammps
+mpirun -np ${SLURM_NTASKS} lmp -in Si_Bulk_Hot_Cold.lammps
 #srun --mpi=pmix_v3 lmp -in in.lj
