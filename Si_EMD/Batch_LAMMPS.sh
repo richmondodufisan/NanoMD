f3bb32e83ef231b75b8c79c4aa7d2093ed682b64
Richmond <alesanmiodufisan2028@u.northwestern.edu>
2025-02-25 19:59:37 -0600

    removing parallelization

--- Si_EMD/Batch_LAMMPS.sh
+++ Si_EMD/Batch_LAMMPS.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH --account=p32089  ## YOUR ACCOUNT pXXXX or bXXXX
-#SBATCH --partition=short  ### PARTITION (buyin, short, normal, etc)
+#SBATCH --partition=normal  ### PARTITION (buyin, short, normal, etc)
 #SBATCH --nodes=5 ## how many computers do you need
-#SBATCH --ntasks-per-node=10 ## how many cpus or processors do you need on each computer
-#SBATCH --time=04:00:00 ## how long does this need to run (remember different partitions have restrictions on this param)
+#SBATCH --ntasks-per-node=1 ## how many cpus or processors do you need on each computer
+#SBATCH --time=48:00:00 ## how long does this need to run (remember different partitions have restrictions on this param)
 #SBATCH --mem-per-cpu=1G ## how much RAM do you need per CPU (this effects your FairShare score so be careful to not ask for more than you need))
 #SBATCH --job-name=Si_thermal_grad  ## When you run squeue -u NETID this is how you can identify the job
 #SBATCH --output=log.lammps ## standard out and standard error goes to this file
