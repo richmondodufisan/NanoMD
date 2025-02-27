c483cfad0f1d93d4ac03dc64443f13085dd50642
Richmond <alesanmiodufisan2028@u.northwestern.edu>
2025-02-27 04:15:29 -0600

    let output be slurm-xx

--- Si_EMD/Batch_LAMMPS.sh
+++ Si_EMD/Batch_LAMMPS.sh
@@ -6,7 +6,6 @@
 #SBATCH --time=4:00:00 ## how long does this need to run (remember different partitions have restrictions on this param)
 #SBATCH --mem-per-cpu=1G ## how much RAM do you need per CPU (this effects your FairShare score so be careful to not ask for more than you need))
 #SBATCH --job-name=Si_thermal_grad  ## When you run squeue -u NETID this is how you can identify the job
-#SBATCH --output=log.lammps ## standard out and standard error goes to this file
 #SBATCH --constraint="[quest8|quest9|quest10|quest11]"
 #SBATCH --exclude=qnode0565,qnode0626,qnode0637,qnode0019,qnode0115,qnode1201
 
