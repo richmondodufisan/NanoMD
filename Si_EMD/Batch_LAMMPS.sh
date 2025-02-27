601bb9b7ad849c0ff079280b152e74a599edde6f
Richmond <alesanmiodufisan2028@u.northwestern.edu>
2025-02-27 04:14:57 -0600

    changed time params

--- Si_EMD/Batch_LAMMPS.sh
+++ Si_EMD/Batch_LAMMPS.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --account=p32089  ## YOUR ACCOUNT pXXXX or bXXXX
 #SBATCH --partition=short  ### PARTITION (buyin, short, normal, etc)
-#SBATCH --nodes=5 ## how many computers do you need
-#SBATCH --ntasks-per-node=10 ## how many cpus or processors do you need on each computer
+#SBATCH --nodes=1 ## how many computers do you need
+#SBATCH --ntasks-per-node=20 ## how many cpus or processors do you need on each computer
 #SBATCH --time=4:00:00 ## how long does this need to run (remember different partitions have restrictions on this param)
 #SBATCH --mem-per-cpu=1G ## how much RAM do you need per CPU (this effects your FairShare score so be careful to not ask for more than you need))
 #SBATCH --job-name=Si_thermal_grad  ## When you run squeue -u NETID this is how you can identify the job
