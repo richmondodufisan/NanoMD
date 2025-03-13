6db7992f97093165bbbda2d3ef79dacb14c49dd2
Richmond Odufisan <alesanmiodufisan@yahoo.com>
2025-03-12 23:00:55 -0500

    removed node constraints

--- Si_EMD/Batch_LAMMPS.sh
+++ Si_EMD/Batch_LAMMPS.sh
@@ -6,8 +6,7 @@
 #SBATCH --time=48:00:00 ## how long does this need to run (remember different partitions have restrictions on this param)
 #SBATCH --mem-per-cpu=1G ## how much RAM do you need per CPU (this effects your FairShare score so be careful to not ask for more than you need))
 #SBATCH --job-name=Si_thermal_grad  ## When you run squeue -u NETID this is how you can identify the job
-#SBATCH --constraint="[quest8|quest9|quest10|quest11]"
-#SBATCH --exclude=qnode0565,qnode0626,qnode0637,qnode0019,qnode0115,qnode1201
+#SBATCH --exclude=qnode0565
 
 module purge
 module load lammps/20200303-openmpi-4.0.5-intel-19.0.5.281
